0ada7133af0a3677aaacaeb678a8fc9ce66c2a0e
Cameron Warner <cameron.w.warner@gmail.com>
2023-11-17 19:37:28 -0500

    Initial Commit

---
+++ Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by Cameron Warner on 11/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
